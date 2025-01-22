bae0a2be517591af581652e1db415b0e03d3985e
MGMerlo07 <mgarmer2811@g.educaand.es>
2025-01-22 07:53:01 +0100

    Subido ejercicio 'agenda' con Css antiguo'

--- agenda/src/app/page.js
+++ agenda/src/app/page.js
@@ -1,15 +1,16 @@
 "use client"
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
     const router = useRouter();
+
+    useEffect(() =>{
+        router.push("/contact")
+    },[])
     
     return (
         <div>
-            <h1>Bro, esta es la pagina principal</h1>
-            <br />
-            <br />
-            <button onClick={() => router.push("/contact")}>Ir a la agenda de contactos</button>
         </div>
     );
 }
